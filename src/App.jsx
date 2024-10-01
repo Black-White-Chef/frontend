5dbb94f88c9701457ef6547ead090ae156beb596
sujin <sjpart3@gmail.com>
2024-10-01 20:00:32 +0900

    black chef data

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,16 @@
+import Header from './components/Header/Header.jsx';
+import { blackData } from './utills/black.js';
+
 function App() {
-  return <>메인</>;
+  return (
+    <>
+      <Header />
+      <div>
+        <h1>{blackData.chef[0].name}</h1>
+        <img src={blackData.chef[0].image} alt={chefData.chef[0].name} />
+      </div>
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,8 @@
+import logoImg from '../../assets/chef/bw-chef-logo.png';
+export default function Header() {
+  return (
+    <header>
+      <img src={logoImg} alt="logo-Img" />
+    </header>
+  );
+}
