52b36ef5018fa7421831b4dbaa6567f7989dbbd8
sujin <sjpart3@gmail.com>
2024-10-01 20:20:43 +0900

    data

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,15 @@ import Header from './components/Header/Header.jsx';
 import { blackData } from './utills/black.js';
 
 function App() {
-  return <>메인</>;
+  return (
+    <>
+      <Header />
+      <div>
+        <h1>{blackData.chef[0].name}</h1>
+        <img src={blackData.chef[0].image} alt={blackData.chef[0].name} />
+      </div>
+    </>
+  );
 }
 
 export default App;
