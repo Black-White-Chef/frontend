e06997afcb6b4040c174dc2772f2cee174d2b984
JunYoung <whwnsdud011@naver.com>
2024-10-06 15:39:55 +0900

    design: 피드백 모달 반응형 적용

--- src/components/FeedbackModal/FeedbackModal.jsx
+++ src/components/FeedbackModal/FeedbackModal.jsx
@@ -1,14 +1,18 @@
-import { useState } from 'react';
-import { useEffect } from 'react';
-import './FeedbackModal.css';
+import { useState } from "react";
+import { useEffect } from "react";
+import "./FeedbackModal.css";
 
 export default function FeedbackModal() {
   const [isModalOpen, setIsModalOpen] = useState(false);
-  const [feedback, setFeedback] = useState('');
+  const [feedback, setFeedback] = useState("");
 
   useEffect(() => {
     const timer = setTimeout(() => {
       setIsModalOpen(true);
+      const scrollBarWidth =
+        window.innerWidth - document.documentElement.clientWidth;
+      document.body.style.overflow = "hidden";
+      document.body.style.paddingRight = `${scrollBarWidth}px`;
     }, 3000);
 
     return () => {
@@ -18,37 +22,41 @@ export default function FeedbackModal() {
 
   const handleFeedback = (e) => {
     e.preventDefault();
-    console.log('Feedback:', feedback);
-    setFeedback('');
+    console.log("Feedback:", feedback);
+    setFeedback("");
     setIsModalOpen(false);
   };
 
   const handleColseModal = () => {
     setIsModalOpen(false);
+    document.body.style.overflow = "";
+    document.body.style.paddingRight = "";
   };
 
   return (
     <>
       {isModalOpen && (
-        <div className="feedbackContainer">
-          <h3>이 사이트에 대한 피드백을 입력해 주세요!</h3>
-          <p>피드백을 입력해 주시면 큰 힘이 됩니다.</p>
-          <form className="feedbackFrom" onSubmit={handleFeedback}>
-            <textarea
-              className="feedbackInput"
-              type="text"
-              value={feedback}
-              onChange={(e) => setFeedback(e.target.value)}
-              placeholder="피드백을 입력해 주세요"
-              required
-            ></textarea>
-            <button className="FeedbackSbtn" type="submit">
-              <span>submit</span>
+        <div className="backdrop">
+          <div className="feedbackContainer">
+            <h3>이 사이트에 대한 피드백을 입력해 주세요!</h3>
+            <p>피드백을 입력해 주시면 큰 힘이 됩니다.</p>
+            <form className="feedbackFrom" onSubmit={handleFeedback}>
+              <textarea
+                className="feedbackInput"
+                type="text"
+                value={feedback}
+                onChange={(e) => setFeedback(e.target.value)}
+                placeholder="피드백을 입력해 주세요"
+                required
+              ></textarea>
+              <button className="FeedbackSbtn" type="submit">
+                <span>submit</span>
+              </button>
+            </form>
+            <button className="FeedbackCbtn" onClick={handleColseModal}>
+              X
             </button>
-          </form>
-          <button className="FeedbackCbtn" onClick={handleColseModal}>
-            X
-          </button>
+          </div>
         </div>
       )}
     </>
