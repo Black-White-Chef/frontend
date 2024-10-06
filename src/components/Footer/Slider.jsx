a6f57547dea7a8821163e9f7c2a673953af4fde3
sujin <sjpart3@gmail.com>
2024-10-06 18:15:45 +0900

    design: 슬라이더 위치 수정

--- src/components/Footer/Slider.jsx
+++ src/components/Footer/Slider.jsx
@@ -13,29 +13,44 @@ const texts = [
   'Text 10',
   'Text 11',
   'Text 12',
+  'Text 11111111111111111111111111111111111111',
+  'Text 2',
+  'Text 3',
+  'Text 4',
+  'Text 5',
+  'Text 6',
+  'Text 7',
+  'Text 8',
+  'Text 9',
+  'Text 10',
+  'Text 11',
+  'Text 12',
 ];
 
 export default function Slider() {
   return (
     <>
-      <div className="slider">
-        <div className="slide-track-t">
-          {texts.concat(texts).map((text, index) => (
-            <div className="slider" key={index}>
-              {text}
-            </div>
-          ))}
+      <div className="slider-wrapper">
+        <div className="slider">
+          <div className="slide-track slide-track-t">
+            {texts.concat(texts).map((text, index) => (
+              <div className="slide" key={index}>
+                {text}
+              </div>
+            ))}
+          </div>
+        </div>
+        <div className="slider">
+          <div className="slide-track slide-track-b">
+            {texts.concat(texts).map((text, index) => (
+              <div className="slide" key={index}>
+                {text}
+              </div>
+            ))}
+          </div>
         </div>
       </div>
-      <div className="slider">
-        <div className="slide-track-b">
-          {texts.concat(texts).map((text, index) => (
-            <div className="slider" key={index}>
-              {text}
-            </div>
-          ))}
-        </div>
-      </div>
+      <div className="gap"></div>
     </>
   );
 }
